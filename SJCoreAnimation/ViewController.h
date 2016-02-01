f9613c2e0e72e4cf542f2ebb654ac452f91e13a0
fushengjun <fushengjun@xiangshang360.com>
2016-02-01 16:05:48 +0800

    Initial Commit

---
+++ SJCoreAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SJCoreAnimation
+//
+//  Created by SPIREJ on 16/2/1.
+//  Copyright © 2016年 SPIREJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SJCoreAnimation/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SJCoreAnimation
+//
+//  Created by SPIREJ on 16/2/1.
+//  Copyright © 2016年 SPIREJ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
